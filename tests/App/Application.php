6e19e218e02facabcd47e15928bada549caa65e4
Pedro Tanaka <pedro.stanaka@gmail.com>
2018-06-25 14:21:36 +0000

    Adding Plugin object to force load of routes file

--- tests/App/Application.php
+++ tests/App/Application.php
@@ -7,6 +7,12 @@ use Cake\Routing\Middleware\RoutingMiddleware;
 
 class Application extends BaseApplication
 {
+    /**
+     * Loads the assets and routing middleware which are necessary to test the plugin.
+     * 
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to set in your App Class
+     * @return \Cake\Http\MiddlewareQueue
+     */
     public function middleware($middlewareQueue)
     {
         $middlewareQueue
